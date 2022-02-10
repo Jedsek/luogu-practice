5d4f7761a684b2dd244cedd7a8aaea486bb87988
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-02-10 18:55:16 +0800

    Create P5712.cpp

---
+++ P5712.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<<"Today, I ate "<<x<<" apple";
+    if(x>1)cout<<"s";
+    cout<<".";
+    return 0;
+}
