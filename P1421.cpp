c327c6dbaf767b2b1595b124a122196dfe4599cc
Jedsek <63626406+Jedsek@users.noreply.github.com>
2022-02-10 18:13:52 +0800

    Create P1421.cpp

---
+++ P1421.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    b = a*10+b;
+    printf("%d",b/19);
+    return 0;
+}
